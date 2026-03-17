b32383bd68ed03748a28accb88d12f62b8a7ed45
CzPhantom10 <ps826105@gmail.com>
2026-03-18 04:16:02 +0530

    Testing secret detection

--- app.py
+++ app.py
@@ -2,10 +2,11 @@
 from flask import Flask
 
 app = Flask(__name__)
+API_KEY = "12345"
 
 @app.route("/")
 def home():
-    return "Secure DevOps Pipeline is Working 🚀"
+    return "Secure DevOps Pipeline is Working "
 
 if __name__ == "__main__":
     app.run(debug=True)
